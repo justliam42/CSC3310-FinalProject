5e58ef197e0f185d0dcd01e44f289111ec55f7d6
Helina Mulugeta <mulugetah@msoe.edu>
2025-12-09 01:06:59 -0600

    Partially fixed bugs
    I've fixed a couple of bugs but I'm still in the process of hashing out others.

--- btree.py
+++ btree.py
@@ -20,7 +20,6 @@ class BTreeNode:
     def leaf(self):
         return len(self.children) == 0
 
-
     def search(self, key):
         # iterate through keys
         for i in range(len(self.keys)+1):
@@ -80,6 +79,8 @@ class BTreeNode:
         if root and len(self.keys) > maxdeg:
             newroot = BTreeNode()
             left,mid,right = self.split()
+
+            # left and right roots should still contain the old children
             newroot.children = [left,right]
             newroot.keys = [mid]
             return newroot
@@ -96,9 +97,15 @@ class BTreeNode:
         left.keys = self.keys[:i_mid]
         right.keys = self.keys[i_mid+1:]
 
+        # children with keys less than the mid key should go to the left
+        left.children = self.children[:i_mid+1]
+
+        # children with keys greater than the mid key shoud go to the right
+        right.children = self.children[i_mid+1:]
+
         return (left,self.keys[i_mid],right)
 
-    def delete(self,key, mindeg):
+    def delete(self,key, mindeg, root=False):
         # NTS: every pop, remove, delete action should be followed by a verification of b tree ness
         # NTS: root node can violate the rule, so it can have just one element
         if self.leaf():
@@ -116,9 +123,11 @@ class BTreeNode:
                     right = self.children[i+1] if i < len(self.children) else None
 
                     if left:
-                        self.keys[i] = left.keys.pop()
+                        self.keys[i] = left.keys[-1]
+                        left.delete(self.keys[i], mindeg)
                     elif right :
-                        self.keys[i] = right.keys.pop(0)
+                        self.keys[i] = right.keys[0]
+                        right.delete(self.keys[i], mindeg)
 
                     break
 
@@ -137,68 +146,75 @@ class BTreeNode:
 
                 # NTS: self.keys[i] is NOT the separator
                 if left and len(left.keys) > mindeg:
-                    # find separator between the left child and child
+                    # find separator between the left sibling and child
                     # it has a left sibling so this is at least 0
-                    sep = bisect.bisect_left(self.keys, child.keys[0]) - 1
+                    sep = 0
+                    if len(child.keys) > 0:
+                        sep = bisect.bisect_left(self.keys, child.keys[0]) - 1
 
 
                     k = left.keys.pop() # largest element from the left
-                    child.keys.insert(0,self.keys[sep]) # separator is smaller than all the keys in the child
+                    child.keys.insert(0,self.keys[i]) # separator is smaller than all the keys in the child
+
+                    # also take the right child  of the left sibling if present
+                    if len(left.children) > 0:
+                        child.children.insert(0, left.children.pop())
+
                     self.keys[sep] = k # move largest element from the left to the parent
                 elif right and len(right.keys) > mindeg:
                     k = right.keys.pop(0)
                     child.keys.append(self.keys[i])
+
+                    # also take the left child of right sibling if present
+                    if len(right.children) > 0:
+                        child.children.insert(0, right.children.pop(0))
                     self.keys[i] = k
                 else:
                     # No siblings have enough keys, we merge two nodes together
                     new_node = BTreeNode()
 
                     # find the separator to be moved down, use smallest element from child
-                    sep = bisect.bisect_left(self.keys, child.keys[0]) - 1
+                    sep = 0
+                    if len(child.keys) > 0:
+                        sep = bisect.bisect_left(self.keys, child.keys[0]) - 1
+                    
+                    middle = self.keys[sep]
 
                     # cases: had a left sibling, had a right sibling, had no siblings
                     if left:
-                        mid_i = sep
-
                         new_node.children = left.children + child.children
-                        new_node.keys = left.keys + [self.keys[mid_i]] + child.keys
-                        self.keys.pop(mid_i)
+                        new_node.keys = left.keys + [middle] + child.keys
+
+                        self.keys.pop(sep)
 
                         # remove the old nodes
                         self.children.remove(left)
                         self.children.remove(child)
 
-                        # if the merge means that we don't have any more keys in the root, the new
-                        # node is the root
-                        if len(self.keys) == 0:
-                            self.children = new_node.children
-                            self.keys = new_node.keys
-                        else:
-                            # add the new node as a child
-                            # TODO: fix where it is being added, it's not always at the end
-                            self.children.insert(i, new_node)
+                        # add the new node as a child
+                        # TODO: fix where it is being added, it's not always at the end
+                        self.children.insert(i, new_node)
 
                     else:
-                        mid_i = sep + 1
                         new_node.children = child.children + right.children
-                        new_node.keys = child.keys + [self.keys[mid_i]] + right.keys
-                        self.keys.pop(mid_i)
+                        new_node.keys = child.keys + [middle] + right.keys
+                        
+                        self.keys.pop(sep)
 
                         # remove the old nodes
                         self.children.remove(right)
                         self.children.remove(child)
 
-                        # if the merge means that we don't have any more keys in the root, the new
-                        # node is the root
-                        if len(self.keys) == 0:
-                            self.children = new_node.children
-                            self.keys = new_node.keys
-                        else:
-                            # add the new node as a child
-                            # TODO: fix where it is being added, it's not always at the end
-                            # i is the index of the separator we are less than, which is now removed
-                            # for a given i, i is the left child of the separator, i + 1 is the right child of the separator
-                            self.children.insert(i , new_node)
+                        # add the new node as a child
+                        # TODO: fix where it is being added, it's not always at the end
+                        # i is the index of the separator we are less than, which is now removed
+                        # for a given i, i is the left child of the separator, i + 1 is the right child of the separator
+                        self.children.insert(i , new_node)
+        if root and len(self.keys) == 0:
+            # if the root is empty, then the tree has reconfigured such that the new root is at level 1
+            # remove empty root and assign level 1 as the new root
+            self.keys = self.children[0].keys
+            self.children = self.children[0].children
 
 
 class BTree:
@@ -214,16 +230,16 @@ class BTree:
         self.root = self.root.insert(key,self.maxdeg)
 
     def delete(self, key):
-        self.root.delete(key, self.mindeg)
+        self.root.delete(key, self.mindeg, True)
 
     def printtree(self):
         self.root.display()
 
 def testbtrees():
     print("Create BTree of minimum degree=2")
-    btree = BTree(2)
+    btree = BTree(1)
     # example from: https://www.youtube.com/watch?v=K1a2Bk8NrYQ
-    keys = [59, 23, 7, 97, 73, 67, 19, 79, 61, 41]
+    keys = [20, 40, 10, 30, 32, 50, 60, 5, 15, 25, 28, 31, 32, 35, 45, 55, 65]
 
     for key in keys:
         btree.insert(key)
@@ -238,7 +254,8 @@ def testbtrees():
     for key in [11,13,0,-1,100,31,29]:
         print("Search key:", key, "Found:",btree.search(key))
 
-    for key in keys:
+    delete = [31, 28, 45, 32]
+    for key in delete:
         btree.delete(key)
         print("Delete key:", key)
         btree.printtree()
